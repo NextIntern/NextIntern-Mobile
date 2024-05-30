4f2091ea591c8e651d104be24762994398d401bb
Hai Dang <contact@hdang09.tech>
2024-05-31 00:14:28 +0700

    Init source code

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_vscode/src/app.dart';
+
+//importaciones de mis archivos
+
+void main() {
+  runApp(MyApp());
+}
---
+++ lib/src/app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_vscode/src/pages/home_page.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(context) {
+    return MaterialApp(
+      home: Center(
+        child: HomePage(),
+      ),
+    );
+  }
+}
